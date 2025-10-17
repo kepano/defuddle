f1245619628cf7ee2c2eb7a3bd49bbb86fd2b032
Dmitry Kury <dima.kurilchenko@gmail.com>
2025-10-17 08:53:18 +0000

    feat(markdown): enforce markdown in index.markdown.ts options

--- src/index.markdown.ts
+++ src/index.markdown.ts
@@ -7,17 +7,16 @@ import { createMarkdownContent } from './markdown';
  * This export includes turndown and enables separateMarkdown by default
  */
 export default class DefuddleWithMarkdown extends Defuddle {
-	constructor(doc: Document, options: DefuddleOptions = {}) {
-		// Enable separateMarkdown by default if not explicitly set
-		const markdownOptions: DefuddleOptions = {
-			separateMarkdown: true,
-			...options,
-			markdownProcessor: createMarkdownContent
-		};
-		super(doc, markdownOptions);
-	}
+  constructor(doc: Document, options: DefuddleOptions = {}) {
+    // Enable separateMarkdown by default if not explicitly set
+    const markdownOptions: DefuddleOptions = {
+      ...options,
+      separateMarkdown: options.separateMarkdown ?? true,
+      markdownProcessor: options.markdownProcessor ?? createMarkdownContent
+    };
+    super(doc, markdownOptions);
+  }
 }
 
 // Re-export types
 export type { DefuddleOptions, DefuddleResponse };
-
