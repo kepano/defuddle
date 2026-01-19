e70a57e040f186e0299c9f34e1ed7c05ba7bfb1d
Dharam Kapila <dharamkapila@gmail.com>
2026-01-19 18:36:47 +0100

    Add support for HTML to markdown conversion in the Full bundle

--- src/index.full.ts
+++ src/index.full.ts
@@ -1,10 +1,35 @@
-import { Defuddle } from './defuddle';
+import { Defuddle as BaseDefuddle } from './defuddle';
+import { createMarkdownContent } from './markdown';
 import { DefuddleOptions, DefuddleResponse } from './types';
 
 // Export types
 export type { DefuddleOptions, DefuddleResponse };
 
-// Export Defuddle as default
+export class Defuddle extends BaseDefuddle {
+	private readonly markdownOptions: DefuddleOptions;
+
+	constructor(document: Document, options: DefuddleOptions = {}) {
+		super(document, options);
+		this.markdownOptions = options;
+	}
+
+	parse(): DefuddleResponse {
+		const result = super.parse();
+
+		if (this.markdownOptions.markdown) {
+			result.content = createMarkdownContent(result.content, this.markdownOptions.url ?? '');
+		} else if (this.markdownOptions.separateMarkdown) {
+			result.contentMarkdown = createMarkdownContent(
+				result.content,
+				this.markdownOptions.url ?? ''
+			);
+		}
+
+		return result;
+	}
+}
+
+// Export Defuddle (full bundle) as default, with markdown support enabled.
 export default Defuddle;
 
 // This file exists to ensure proper type generation for the full bundle
