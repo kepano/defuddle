1270bb4443f6c6d076e711d355dd5598895b729a
Steph Ango <10565871+kepano@users.noreply.github.com>
2026-03-04 15:45:31 -0800

    Tests

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+import path from 'path';
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			'./elements/math': path.resolve(__dirname, 'src/elements/math.full.ts'),
+		},
+	},
+});
